d72f494c31c94ec84ca2997c275c452226c63423
Alexander Novikov <nvkv_ai@outlook.com>
2022-06-24 16:01:29 +0300

    clean up

--- StackedWallsTransform/StackedWallsTransform.py
+++ StackedWallsTransform/StackedWallsTransform.py
@@ -1,21 +1,21 @@
-import clr #необходимо для модуля библиотек .NET
-clr.AddReference('RevitAPI') #ссылка на библиотеку dll
-from Autodesk.Revit.DB import * #импорт всех классов
+import clr
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
 
-clr.AddReference('RevitServices') #импорт библиотеки для загрузки классов DM и TM
+clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager as DM
 from RevitServices.Transactions import TransactionManager as TM
 
-doc=DM.Instance.CurrentDBDocument #ссылка на текущий документ
+doc=DM.Instance.CurrentDBDocument
 
-SW=FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StackedWalls).WhereElementIsNotElementType().ToElements() #получение всех стен в проекте
+SW=FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StackedWalls).WhereElementIsNotElementType().ToElements()
 
-WI=[] #выбор id стен, входящих в состав составных
+WI=[]
 
 for i in SW :
 	WI.append(i.GetStackedWallMemberIds())
 	
-W=[] #выбор стен, входящих в состав составных
+W=[]
 
 for i in WI:
 	for j in i:
@@ -25,16 +25,16 @@ TM.Instance.EnsureInTransaction(doc)
 
 if IN[0]==True:
 	for i in W:	
-		H=int(i.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsValueString().split(',')[0])/304.8 #получение высоты элемента и переведенное в мм (так как в API используются футы)			
-		O=int(i.get_Parameter(BuiltInParameter.WALL_BASE_OFFSET).AsValueString().split(',')[0])/304.8 #получение смещения				
-		Wall.Create(doc,i.Location.Curve,i.WallType.Id,i.LevelId,H,O,i.Flipped,False) #создание стен		
+		H=int(i.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsValueString().split(',')[0])/304.8		
+		O=int(i.get_Parameter(BuiltInParameter.WALL_BASE_OFFSET).AsValueString().split(',')[0])/304.8
+		Wall.Create(doc,i.Location.Curve,i.WallType.Id,i.LevelId,H,O,i.Flipped,False)
 	OUT="Created "+str(len(W))+" Walls."	
 else:
 	OUT="<True> is expected."
 	
 if IN[1]==True:
 	for i in SW:
-		doc.Delete(i.Id) #удаление стен
+		doc.Delete(i.Id)
 	OUT="Deleted "+str(len(SW))+" Stacked Walls"
 
 if IN[0]*IN[1]==True:
